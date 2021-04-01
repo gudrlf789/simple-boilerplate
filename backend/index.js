bb208d324570f12bea8a83514ad1c00783062a69
HyeongGiri <gudrlf46@gmail.com>
2021-04-02 00:09:00 +0900

    npm init 및 index.js 파일 생성

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,18 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-app.get('/', (req, res) => res.send('Hello World'))
+const db = require('./config/keys')
+
+const mongoose = require('mongoose');
+// 몽고db에 연결
+mongoose.connect(db.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err))
+
+  app.get('/', (req, res) => res.send('Hello World'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
