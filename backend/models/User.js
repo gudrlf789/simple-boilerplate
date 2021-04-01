91af54274697a99d4f6e0ba2be454b85b445dff4
HyeongGiri <gudrlf46@gmail.com>
2021-04-02 00:32:56 +0900

    models 폴더 생성후 model, schema 관련 파일 생성

---
+++ backend/models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
+    },
+    password: {
+        type: String,
+        minlength: 5
+    },
+    lastname: {
+        type: String,
+        maxlength: 50
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    image: String,
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+});
+
+// Model은 스키마를 감싸주는 역할
+// model(모델이름, 스키마)
+const User = mongoose.model('User', userSchema)
+
+module.exports = {User}
