adb38aefb624f787a42152ad03e6809e6c9ca006
HyeongGiri <gudrlf46@gmail.com>
2021-04-04 13:12:17 +0900

    bodyparser 설치민 간단한 테스트 코드 작성

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,14 @@ const port = 5000
 
 const db = require('./config/keys')
 
+const bodyParser = require('body-parser')
+const { User } = require('./models/User');
+
+//application/x-www-form-urlencoded 를 분석해서 가져올수 있게 도와준다.
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json 타입을 분석해서 가져올수 있게 도와준다.
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose');
 // 몽고db에 연결
 mongoose.connect(db.mongoURI, {
@@ -15,5 +23,19 @@ mongoose.connect(db.mongoURI, {
   .catch(err => console.log(err))
 
   app.get('/', (req, res) => res.send('Hello World'))
+
+  app.post('/register', (req, res) => {
+
+    //회원 가입 할때 필요한 정보들을 client에서 가져오면 그 정보들을 db에 넣어준다.
+    const user = new User(req.body);
+
+    user.save((err, userInfo) => {
+      if(err) return res.json({ sucess: false, err })
+
+      return res.status(200).json({
+        sucess: true
+      })
+    })
+  })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
