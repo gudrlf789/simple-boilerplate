706d1de3e6d81205d7e07186c3ae2d118e82aa5d
HyeongGiri <gudrlf46@gmail.com>
2021-04-14 23:59:18 +0900

    포트 변수 위치 수정

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
-const app = express()
-const port = 5000
+const app = express();
 
 const db = require('./config/keys')
 
@@ -28,6 +27,10 @@ mongoose.connect(db.mongoURI, {
   .catch(err => console.log(err))
 
   app.get('/', (req, res) => res.send('Hello World'))
+
+  app.get('/api/hello', (req, res) => {
+    res.send("안녕하세요");
+  })
 
   app.post('/api/users/register', (req, res) => {
 
@@ -95,4 +98,5 @@ mongoose.connect(db.mongoURI, {
     })
   })
 
+const port = 5000;
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
