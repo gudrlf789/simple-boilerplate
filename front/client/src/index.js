e5d26fedc92261dc74a9a29f4b3611119f488880
HyeongGiri <gudrlf46@gmail.com>
2021-05-23 00:23:37 +0900

    redux react-redux redux-promise redux-thunk antd 설치

--- front/client/src/index.js
+++ front/client/src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux';
 import 'antd/dist/antd.css';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
