70d8e882ac789b721e5af2de9fd1ff18c0cd928c
HyeongGiri <gudrlf46@gmail.com>
2021-05-23 00:37:03 +0900

    전체 reducer를 관리하는 rootReducer를 만든다.

---
+++ front/client/src/_reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+//import user from './user_reducer';
+
+// redux의 combineReducers를 이용해서 전체 reducer를 관리하는 rootReducer를 만든다.
+const rootReducer = combineReducers({
+
+})
+
+export default rootReducer;
